d66a41ef14b914578e2374f2e13fd6e7ef803b62
hulaanmo <inhog.cedrick@gmail.com>
2026-08-15 20:55:48 +0800

    feat: add endpoint to retrieve chat memory by conversation ID in MemoryController

--- src/main/java/com/example/learningspringai/memory/MemoryController.java
+++ src/main/java/com/example/learningspringai/memory/MemoryController.java
@@ -4,19 +4,20 @@ package com.example.learningspringai.memory;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ai.chat.messages.Message;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/memory")
 public class MemoryController {
 
     private final ChatClient chatClient;
-
+    private final ChatMemory chatMemory;
 
     public MemoryController(ChatClient.Builder builder, ChatMemory chatMemory) {
+        this.chatMemory = chatMemory;
         this.chatClient = builder
                 .defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build())
                 .build();
@@ -29,6 +30,11 @@ public class MemoryController {
                 .advisors(a -> a.param(ChatMemory.CONVERSATION_ID, "007"))
                 .call()
                 .content();
+    }
+
+    @GetMapping("/{conversationId}")
+    public List<Message> getMemory(@PathVariable("conversationId") String id) {
+        return chatMemory.get(id);
     }
 
 
